31a97e678aee9294316bfe958462c91349ab9a6a
joshrocheios <joshrocheios@gmail.com>
2024-03-20 11:41:23 +0000

    Added project files and views

---
+++ HIITFit/HIITFit/Views/ExerciseView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct ExerciseView: View {
+  
+  let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
+  let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+  
+  let index: Int
+  
+  var body: some View {
+        Text(exerciseNames[index])
+    }
+}
+
+#Preview {
+  ExerciseView(index: 0)
+}
